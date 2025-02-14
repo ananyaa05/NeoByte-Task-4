ecee2cc79bbd1ee547076f63ea777e6e4c5ae426
ananyaa05 <87243811+ananyaa05@users.noreply.github.com>
2025-02-14 22:06:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ const PORT = 3000;
 const SECRET = 'mysecret';
 app.use(express.json());
 let users = [];
+
+function authenticate(req, res, next) {
+  const token = req.headers['authorization'];
+  if (!token) return res.sendStatus(401);
+  jwt.verify(token, SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.post('/login', (req, res) => {
+  const { username } = req.body;
+  const token = jwt.sign({ username }, SECRET, { expiresIn: '1h' });
+  res.json({ token });
+});
+
+app.post('/users', authenticate, (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.status(201).json(user);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
